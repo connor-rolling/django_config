f109ff820dfb3665e425329a139ab805941042f8
Connor Rolling <connor.rolling@snhu.edu>
2022-10-13 14:02:39 -0400

    Initial commit

---
+++ pages/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.http import HttpResponse
+
+from django.views.generic import TemplateView
+
+
+def homePageView(request):
+    return HttpResponse('Hello World!')
+
+
+class HomePageView(TemplateView):
+    template_name = "pages/home.html"
+
+
+class AboutPageView(TemplateView):
+    template_name = "pages/about.html"
